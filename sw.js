73b0606d1dc37bda692706efbbcbc25c2e6ea02c
keekxun <keekxun@gmail.com>
2026-07-05 23:30:37 +0100

    make order size customizable per pair instead of global, bump sw to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'arb-multi-v7';
+const CACHE = 'arb-multi-v8';
 const SHELL = ['./arb-bot-multi-currency.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
